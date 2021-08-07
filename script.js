e367b2433230dbe47dad91f530dce43962082ee3
Franz <francesco.romeo-work@libero.it>
2021-08-07 02:31:52 +0200

    Completed JS code

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-let load = 0
+let load = 0;
 
-let interval = setInterval(blurring, 30)
+let interval = setInterval(blurring, 30);
 
 function blurring() {
-    load++
+    load++;
 
     if(load > 99){
-        clearInterval(interval)
+        clearInterval(interval);
     }
 
-    console.log(load)
-    loadText.inneeText = `${load}%`
-    loadText.getElementsByClassName.opacity = 1;
+    console.log(load);
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 0, 1);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
